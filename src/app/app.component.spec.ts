d993a1b498f4b67c2ddbe2fdbfbf638851469da4
Theo <tspt89@hotmail.com>
2020-09-22 01:20:29 -0500

    test fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
   it(`should have as title 'bigdata-maps'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bigdata-msdfsfdsfaps');
+    expect(app.title).toEqual('bigdata-msdfdsfaps');
   }));
   it(' -- should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
